b5f3c2f235eb94f279f5365dec3b713ea956a976
Antonio Amate <4252008@alu.murciaeduca.es>
2023-11-10 21:04:12 +0100

    background y cuadrado funcionando

--- main.js
+++ main.js
@@ -1,39 +1,69 @@
 onload = () => {
   const canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
+  const sharedContext = {
+    ctx: ctx,
+  };
 
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-
-  const player = new Player({ x: 50, y: 50 });
   const keys = new Keys();
+
   const background = new Background({
     position: { x: 0, y: 0 },
-    imageSrc: "./img/background.png",
-    sourceRect: { x: 0, y: 0, width: canvas.width, height: canvas.height },
-    scale: 1,
+    keys,
+    sharedContext,
   });
-
-  function animation(ctx) {
+  
+  const player = new Player({
+    position: { x: 50, y: 50 },
+    keys,
+    sharedContext,
+  });
+  
+  function animation() {
+    //limpiar el canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.table(keys)
 
+    background.update();
+    player.update();
+    
     background.draw(ctx);
-    player.draw(ctx)
+    player.draw(ctx);
   }
 
   addEventListener("keydown", (e) => {
     switch (e.key) {
       case "w":
-        keys.w.pressed = true;
+        keys.w = true;
         break;
       case "a":
-        keys.a.pressed = true;
+        keys.a = true;
         break;
       case "s":
-        keys.s.pressed = true;
+        keys.s = true;
         break;
       case "d":
-        keys.d.pressed = true;
+        keys.d = true;
+        break;
+      case "ArrowUp":
+        keys.arrowUp = true;
+        break;
+      case "ArrowDown":
+        keys.arrowDown = true;
+        break;
+      case "ArrowLeft":
+        keys.arrowLeft = true;
+        break;
+      case "ArrowRight":
+        keys.arrowRight = true;
+        break;
+      case "+":
+        keys.zoomIn = true;
+        break;
+      case "-":
+        keys.zoomOut = true;
         break;
     }
   });
@@ -41,20 +71,45 @@ onload = () => {
   addEventListener("keyup", (e) => {
     switch (e.key) {
       case "w":
-        keys.w.pressed = false;
+        keys.w = false;
         break;
       case "a":
-        keys.a.pressed = false;
+        keys.a = false;
         break;
       case "s":
-        keys.s.pressed = false;
+        keys.s = false;
         break;
       case "d":
-        keys.d.pressed = false;
+        keys.d = false;
+        break;
+      case "ArrowUp":
+        keys.arrowUp = false;
+        break;
+      case "ArrowDown":
+        keys.arrowDown = false;
+        break;
+      case "ArrowLeft":
+        keys.arrowLeft = false;
+        break;
+      case "ArrowRight":
+        keys.arrowRight = false;
+        break;
+      case "+":
+        keys.zoomIn = false;
+        break;
+      case "-":
+        keys.zoomOut = false;
         break;
     }
   });
+  background.image.onload = () => {
+    // Usar requestAnimationFrame para la animación en lugar de setInterval
+    function animate() {
+      animation();
+      requestAnimationFrame(animate);
+    }
 
-  let id1 = setInterval(animation(ctx), 1000 / 50);
-  let id2 = setInterval(() => player.walk(), 1000 / 1);
+    animate(); // Iniciar la animación
+  };
 };
+
--- model.js
+++ model.js
@@ -1,72 +1,88 @@
 class Background {
-    constructor({ position, imageSrc, sourceRect = null, scale = 1 }) {
+    constructor({ position, keys, sharedContext }) {
       this.position = position;
+      this.keys = keys;
+      this.sharedContext = sharedContext;
       this.image = new Image();
-      this.image.src = imageSrc;
-      this.sourceRect = sourceRect;
-      this.scale = scale;
+      this.image.src = "./img/background.png";
+      this.zoomSpeed = 1.01;
+      this.speed = 10;
+      this.zoom = this.maxZoom = Math.max(canvas.width / this.image.width, canvas.height / this.image.height);
+      this.target = {
+        x: this.image.height * this.zoom,
+        y: this.image.width * this.zoom,
+      };
     }
+    draw() {
+      this.update();
+      this.validarZoom();
   
-    draw(ctx) {
-        ctx.drawImage(
-          this.image,
-          this.sourceRect.x, this.sourceRect.y, this.sourceRect.width, this.sourceRect.height,
-          this.position.x, this.position.y,
-          this.sourceRect.width * this.scale, this.sourceRect.height * this.scale
-        );
+      this.sharedContext.ctx.drawImage(this.image, -this.position.x, -this.position.y, this.target.x, this.target.y);
     }
-  
     update() {
-
-
+      if (this.keys.arrowLeft) this.position.x -= this.speed;
+      if (this.keys.arrowRight) this.position.x += this.speed;
+      if (this.keys.arrowDown) this.position.y += this.speed;
+      if (this.keys.arrowUp) this.position.y -= this.speed;
+      if (this.keys.zoomIn) this.zoom = this.zoom * this.zoomSpeed
+      if (this.keys.zoomOut) this.zoom = this.zoom / this.zoomSpeed
+    }
+    validarZoom() {
+      if (this.zoom < this.maxZoom) this.zoom = this.maxZoom;
+      this.target.y = this.image.height * this.zoom;
+      this.target.x = this.image.width * this.zoom;
+      if (this.position.x + canvas.width > this.target.x) this.position.x = this.target.x - canvas.width;
+      if (this.position.y + canvas.height > this.target.y) this.position.y = this.target.y - canvas.height;
+      if (this.position.x < 0) this.position.x = 0;
+      if (this.position.y < 0) this.position.y = 0;
     }
   }
   
-class Player {
-  constructor(position) {
-    this.position = position;
-    this.velocity = {
-      x: 0,
-      y: 0,
-    };
-    this.size = {
-      width: 100,
-      height: 100,
-    };
-    this.covered = false;
-    this.onGround = false;
-  }
-  cover(state) {
-    this.covered = state;
-  }
-  draw(ctx) {
-    ctx.fillStyle = "red";
-    ctx.fillRect(
-      this.position.x,
-      this.position.y,
-      this.size.width,
-      this.size.height
-    );
-  }
-  update() {
-
-
-  }
-  jump() {
-    if (this.onGround) {
-      this.velocity.y=-15
+  class Player {
+    constructor({position, keys, sharedContext}) {
+      this.position = position;
+      this.keys = keys;
+      this.sharedContext = sharedContext;
+      this.speed = 10;
+      this.size = {
+        width: 100,
+        height: 100,
+      };
+      this.covered = false;
+      this.onGround = false;
+    }
+    draw() {
+      this.sharedContext.ctx.fillStyle = "red";
+      this.sharedContext.ctx.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
+    }
+    update() {
+      if (this.keys.w) this.position.y -= this.speed;
+      if (this.keys.a) this.position.x -= this.speed;
+      if (this.keys.s) this.position.y += this.speed;
+      if (this.keys.d) this.position.x += this.speed;
+    }
+    jump() {
+      if (this.onGround) {
+        this.velocity.y = -15;
+      }
+    }
+    walk() {
+      console.log("andar");
     }
   }
-  walk(){
-    console.log("andar")
-
+  
+  class Keys {
+    constructor() {
+      this.w = false;
+      this.a = false;
+      this.s = false;
+      this.d = false;
+      this.arrowUp = false;
+      this.arrowDown = false;
+      this.arrowLeft = false;
+      this.arrowRight = false;
+      this.zoomIn = false;
+      this.zoomOut = false;
+    }
   }
-}
-class Keys {
-  constructor() {
-    this.w = false;
-    this.a = false;
-    this.s = false;
-    this.d = false;
-  }
-}
+  
