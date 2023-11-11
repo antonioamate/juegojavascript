0961d37a38501012ad07c9c31015ad298ca603a4
Antonio Amate <4252008@alu.murciaeduca.es>
2023-11-12 00:14:11 +0100

    personaje con fondo y barreras invisibles

--- main.js
+++ main.js
@@ -4,59 +4,67 @@ onload = () => {
   const backgroundImage = new Image();
   backgroundImage.src = "./img/forest.jpg";
 
-  ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-
-  const player = new Player({ position: { x: 50, y: 50 }, ctx });
-
-  function animation() {
-    //limpiar el canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    player.update();
-    player.draw();
-  }
-
-  addEventListener("keydown", (e) => {
-    switch (e.key) {
-      case "w":
-        player.keys.w = true;
-        break;
-      case "a":
-        player.keys.a = true;
-        break;
-      case "s":
-        player.keys.s = true;
-        break;
-      case "d":
-        player.keys.d = true;
-        break;
-    }
-  });
-
-  addEventListener("keyup", (e) => {
-    switch (e.key) {
-      case "w":
-        player.keys.w = false;
-        break;
-      case "a":
-        player.keys.a = false;
-        break;
-      case "s":
-        player.keys.s = false;
-        break;
-      case "d":
-        player.keys.d = false;
-        break;
-    }
-  });
   backgroundImage.onload = () => {
+    // Draw the background image when it's loaded
+
+    const player = new Player({ position: { x: 50, y: 50 }, ctx });
+
+    function animation() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+      console.log("onGround "+player.onGround)
+      
+      player.update();
+
+      player.draw();
+    }
+
+    addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "w":
+          player.keys.w = true;
+          break;
+        case "a":
+          player.keys.a = true;
+          break;
+        case "s":
+          player.keys.s = true;
+          break;
+        case "d":
+          player.keys.d = true;
+          break;
+      }
+    });
+
+    addEventListener("keyup", (e) => {
+      switch (e.key) {
+        case "w":
+          player.keys.w = false;
+          break;
+        case "a":
+          player.keys.a = false;
+          break;
+        case "s":
+          player.keys.s = false;
+          break;
+        case "d":
+          player.keys.d = false;
+          break;
+      }
+    });
+
     function animate() {
       animation();
       requestAnimationFrame(animate);
     }
+
+    // Start the animation loop
     animate();
   };
 };
+
+// The rest of your Player class remains unchanged
 
 class Player {
   constructor({ position, ctx }) {
@@ -70,17 +78,17 @@ class Player {
     };
     this.image = new Image();
     this.image.src = "./img/spritestick.png";
-    this.acceleration = 6;
     this.speed = {
       x: 0,
       y: 0,
     };
     this.size = {
-      width: 200,
-      height: 250,
+      width: 100,
+      height: 125,
     };
-    this.jumpStrength = 25;
-    this.gravity = 1;
+    this.acceleration = 3;
+    this.jumpStrength = 12;
+    this.gravity = 0.3;
     this.animation = 2;
     this.frame = 0;
     this.slowFrame = 0;
@@ -89,18 +97,18 @@ class Player {
     this.facingRight = true;
   }
   draw() {
-    this.ctx.fillStyle = "rgba(255, 0, 0,.5)";
-    this.ctx.fillRect(this.position.x, this.position.y, 200, 250);
+    /* this.ctx.fillStyle = "rgba(255, 0, 0,.5)";
+    this.ctx.fillRect(this.position.x, this.position.y, 100, 125); */
     this.ctx.drawImage(
       this.image,
-      this.frame * 200, //por donde empieza a recortar la imagen
-      this.animation * 250,
-      200, //lo que recorta de la imagen fuente
-      250,
+      this.frame * 100, //por donde empieza a recortar la imagen
+      this.animation * 125,
+      100, //lo que recorta de la imagen fuente
+      125,
       this.position.x,
       this.position.y,
-      200,
-      250
+      100,
+      125
     );
   }
 
@@ -135,19 +143,27 @@ class Player {
     if (this.keys.w) {
       if (this.animation == 4) {
         this.animation = 3;
-      } else if(this.animation==5) {
+      } else if (this.animation == 5) {
         this.animation = 2;
       }
-      
+
       this.jump();
     }
 
     //actualizar posición
     this.speed.y += this.gravity;
-    if (this.position.y + this.speed.y > 470) {
-      this.position.y = 470;
+    if (this.position.y + this.speed.y > 414) {
+      this.position.y = 414;
       this.speed.y = 0;
       this.onGround = true;
+    }
+    if (this.position.x + this.speed.x < 0) {
+      this.speed.x = 0;
+      this.position.x = 0;
+    }
+    if (this.position.x + this.speed.x < 0) {
+      this.speed.x = 0;
+      this.position.x = 0;
     }
     this.position.y += this.speed.y;
     this.position.x += this.speed.x;
