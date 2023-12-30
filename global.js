fcbe103a4fa927252296945d52aaa9d79a25260a
antonioamate <4252008@alu.murciaeduca.es>
2023-12-30 12:39:30 +0100

    Revert "fin bien"
    This reverts commit 915c371f4c540f432917afa1120d332d86dfeb07.

--- global.js
+++ global.js
@@ -346,11 +346,6 @@ function checkEnemyCollisions(enemy) {
 }
 
 function updateRecords() {
-	let tableRows = table.getElementsByTagName('tr')
-    while (tableRows.length > 1) {
-        table.removeChild(tableRows[1]);
-    }
-
 	//aqui obtenemos los records del localstorage
 	let recordsStorage = JSON.parse(localStorage.getItem("records")) || [];
 	//si no hay records en el localstorage no se añaden al array records
